6d24b64785213597f1666ab2b6e1436c8cfdb739
Merve UĞURSAÇ <merve.ugursac34@gmail.com>
2023-07-08 20:52:09 +0300

    Add : Add StackExchangeRedis API and implement Program.cs with new Project

---
+++ RedisExchangeAPI.Web/Services/RedisService.cs
@@ -0,0 +1,34 @@
+﻿using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Services
+{
+    public class RedisService
+    {
+        private readonly string _redisHost;
+        private readonly string _redisPort;
+
+        private ConnectionMultiplexer _redis;
+        public IDatabase db { get; set; } // üzerinden redis-cli ile kullandığımız tüm methodlara erişebiliyoruz
+        public RedisService(IConfiguration configuration)
+        {
+            _redisHost = configuration["Redis:Host"];
+            _redisPort = configuration["Redis:Port"];
+        }
+
+        public void Connect()
+        {
+            var configString = $"{_redisHost}:{_redisPort}";
+
+            _redis = ConnectionMultiplexer.Connect(configString);
+
+            // redis desktop üzerinde 15 tane db vardı
+
+
+        }
+
+        public IDatabase GetDb(int db)
+        {
+            return _redis.GetDatabase(db);
+        }
+    }
+}
