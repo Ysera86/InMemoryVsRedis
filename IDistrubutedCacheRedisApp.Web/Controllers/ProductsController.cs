79cfa21577435ff83964b2a85a8c767dc5d55ca5
Merve UĞURSAÇ <merve.ugursac34@gmail.com>
2023-07-08 20:35:08 +0300

    initial

---
+++ IDistrubutedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,171 @@
+﻿using IDistrubutedCacheRedisApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+using System.Text.Json;
+
+namespace IDistrubutedCacheRedisApp.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IDistributedCache _distributedCache;
+
+        public ProductsController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        //public IActionResult Index()
+        //{
+        //    /*
+        //      127.0.0.1:6379> KEYS * > 
+        //     */
+        //    DistributedCacheEntryOptions cacheOptions = new DistributedCacheEntryOptions();
+        //    cacheOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+
+        //    _distributedCache.SetString("name", "Merve", cacheOptions);
+
+        //    /*
+        //     127.0.0.1:6379> KEYS *
+        //    name        
+
+        //     type name > hash
+
+        //    hget name data > Merve
+
+        //     */
+
+        //    return View();
+        //}
+
+        public async Task< IActionResult > Index()
+        {
+            /*
+              127.0.0.1:6379> KEYS * > 
+             */
+            DistributedCacheEntryOptions cacheOptions = new DistributedCacheEntryOptions();
+            cacheOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+
+            await _distributedCache.SetStringAsync("surname", "Ugursac", cacheOptions);
+
+            /*
+            127.0.0.1:6379> KEYS *
+            surname
+            127.0.0.1:6379> hget surname data
+            Ugursac
+            127.0.0.1:6379>
+
+             */
+
+            #region 1. yol json str
+
+            Product product = new Product { Id = 1, Name = "Kalem", Price = 100 };
+            var jsonProduct = JsonSerializer.Serialize(product);
+
+            await _distributedCache.SetStringAsync("product:1", jsonProduct, cacheOptions);
+
+            #endregion
+
+            #region 2. yol binary str
+
+            Product product2 = new Product { Id = 2, Name = "Kitap", Price = 200 };
+            var jsonProduct2 = JsonSerializer.Serialize(product2);
+
+            Byte[] byteProduct = Encoding.UTF8.GetBytes(jsonProduct2);
+
+            await _distributedCache.SetAsync("product:2", byteProduct, cacheOptions);
+
+            #endregion
+
+            /*
+             KEYS *
+            product:2
+            product:1
+            surname
+
+            127.0.0.1:6379> hget product:1 data
+            {"Id":1,"Name":"Kalem","Price":100}
+            127.0.0.1:6379> hget product:2 data
+            {"Id":2,"Name":"Kitap","Price":200}
+             
+             */
+
+
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            ViewBag.name = _distributedCache.GetString("name");
+
+            //ViewBag.product1 = _distributedCache.GetString("product:1");
+
+            var pjson = _distributedCache.GetString("product:1");
+            Product product = JsonSerializer.Deserialize<Product>(pjson);
+
+            Byte[] bytePjson = _distributedCache.Get("product:2");
+            var pjson2 = Encoding.UTF8.GetString(bytePjson);
+            Product product2 = JsonSerializer.Deserialize<Product>(pjson2);
+
+            ViewBag.product = product;
+            ViewBag.product2 = product2;
+            /*
+             KEYS *
+            product:1
+            surname
+
+            hget product:1 data
+            {"Id":1,"Name":"Kalem","Price":100}  
+            hget surname data
+            Ugursac
+            */
+
+            return View();
+        }
+
+        public IActionResult Remove()
+        {
+            _distributedCache.Remove("name");
+            /*
+              hget name data > 
+              KEYS * > 
+             */
+
+            _distributedCache.Remove("product:1");
+            /*
+              KEYS *
+                surname
+             */
+
+            ViewBag.name = _distributedCache.GetString("name");
+            ViewBag.product1 = _distributedCache.GetString("product:1");
+
+
+
+            return View();
+        }
+  
+        public IActionResult ImageCache()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/Impala.png");
+            byte[] imgByte = System.IO.File.ReadAllBytes(path);
+
+            _distributedCache.Set("image", imgByte);
+
+
+
+            return View();
+        }
+
+        public IActionResult ImageCacheShow()
+        {
+            byte[] imgByte = _distributedCache.Get("image");
+
+            return File(imgByte, "image/png");
+         
+       
+        }
+
+
+    }
+}
---
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -0,0 +1,95 @@
+﻿using InMemoryApp.Web.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryApp.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IMemoryCache _memoryCache;
+        public ProductController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public IActionResult Index()
+        {
+            // cache te her saman key-value
+
+            #region yollar
+
+            //// 1.yol
+            //if (string.IsNullOrEmpty(_memoryCache.Get<string>("time")))
+            //{
+            //    _memoryCache.Set<string>("time", DateTime.Now.ToString());
+            //}
+
+            //// 2.yol
+            //if (!_memoryCache.TryGetValue("time", out string timeCache))
+            //{
+            //    _memoryCache.Set<string>("time", DateTime.Now.ToString());
+            //}
+
+            //// 3. yol
+            //_memoryCache.GetOrCreate<string>("time", x =>
+            //{
+            //    //x.SlidingExpiration = TimeSpan.FromDays(1);
+            //    return DateTime.Now.ToString();
+            //});
+
+            ////timeCache 
+            ///
+
+            //_memoryCache.Remove("time");
+            #endregion
+
+            //IMemoryCache verilen objeleri kendisi seralize ediyor, Redis'te biz yapmalıyız.
+
+            MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+            options.AbsoluteExpiration = DateTime.Now.AddSeconds(10);
+
+            //// 10 snyede 1 erişilirse kalır, erişilmezse silinir ama AbsoluteExpiration verdiğimiz için, erişilse de erşilmese de 1 dk snr silinir.
+            //// bu nedenle SlidingExpiration verilirken AbsoluteExpiration da verilmeli, yoksa bi süre snr bayat dataya erişilir.
+            //options.SlidingExpiration = TimeSpan.FromSeconds(10);
+            //options.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+
+            // mempory key-value mory doldurursa framework silme sırasını nasıl ayarlar ? öncelik sırasına bakar. önem sırası
+            //options.Priority = CacheItemPriority.Low;
+            //options.Priority = CacheItemPriority.Normal;
+            //options.Priority = CacheItemPriority.High;
+            //options.Priority = CacheItemPriority.NeverRemove; // hepsini u seçersek ve mem dolarsa exception fırlatılır.
+            options.Priority = CacheItemPriority.High;
+
+            options.RegisterPostEvictionCallback((key, value, reason, state) => 
+            {
+                _memoryCache.Set<string>("callback", $"{key} : {value}, reason : {reason}, state : {state}");
+            });
+
+            _memoryCache.Set<string>("time", DateTime.Now.ToString(), options);
+
+            //
+            Product p = new () { Id = 1, Name = "kalem", Price = 11 };
+            _memoryCache.Set<Product>("product:1", p);
+
+
+            return View();
+        }
+        public IActionResult Show()
+        {
+            _memoryCache.TryGetValue("time", out string timeCache);
+            ViewBag.time = timeCache;
+
+            _memoryCache.TryGetValue("callback", out string callbackCache);
+            ViewBag.callback = callbackCache;
+
+            _memoryCache.TryGetValue("product:1", out Product productCache);
+            ViewBag.product = productCache;
+
+            //ViewBag.time = _memoryCache.Get<string>("time");
+
+            return View();
+        }
+    }
+}
